4bc1cc48406a181879716471f25391349be04bae
Ubuntu <ubuntu@heaerieglobalsolutions.com>
2019-03-07 04:13:52 +0000

    docker

--- idb/libs/mailcsp.js
+++ idb/libs/mailcsp.js
@@ -5,7 +5,6 @@ var log         = require('./log')(module);
 var config      = require('./config');
 var crypto      = require('crypto');
 var Schema = mongoose.Schema;
-module.exports.mongoose=mongoose;
 
 /* Generated from MAILCSP001.sql by n.js  Don't change */
 /* All copyrights © 2016 Heaerie Global solutions (P) Ltd.India  */
@@ -14,17 +13,17 @@ module.exports.mongoose=mongoose;
 , dtCreated: { type: Date, unique: false, required: false, default: ''  } 
 , athId: { type: Number, unique: false, required: false, default: ''  } 
 , dtModified: { type: Date, unique: false, required: false, default: ''  } 
-, uuid: { type: Number, unique: false, required: false, default: ''  } 
-, msgId: { type: Number, unique: false, required: false, default: ''  } 
-, replyId: { type: Number, unique: false, required: false, default: ''  } 
-, linkId: { type: Number, unique: false, required: false, default: ''  } 
-, subject: { type: Date, unique: false, required: false, default: ''  } 
-, shortBody: { type: String, unique: false, required: false, default: ''  } 
-, body: { type: String, unique: false, required: false, default: ''  } 
+, uuid: { type: String, unique: false, required: false, default: ''  } 
+, msgId: { type: String, unique: false, required: false, default: ''  } 
+, replyId: { type: String, unique: false, required: false, default: ''  } 
+, linkId: { type: String, unique: false, required: false, default: ''  } 
+, subject: { type: String, unique: false, required: false, default: ''  } 
+, shortBody: []
+, body: []
 , receivedDate: { type: Date, unique: false, required: false, default: ''  } 
 , sentDate: { type: Date, unique: false, required: false, default: ''  } 
-, fromList: { type: Number, unique: false, required: false, default: ''  } 
-, toList: { type: Number, unique: false, required: false, default: ''  } 
+, fromList: []
+, toList: []
 , isNewMail: { type: Boolean, unique: false, required: false, default: ''  } 
 , isFlag: { type: Boolean, unique: false, required: false, default: ''  } 
 , grpId: { type: Number, unique: false, required: false, default: ''  } 
@@ -33,4 +32,3 @@ module.exports.mongoose=mongoose;
 
  var MAILCSP_MAIL001MT_Model = mongoose.model('MAILCSP_MAIL001MT', MAILCSP_MAIL001MT); 
  module.exports.MAILCSP_MAIL001MT_Model = MAILCSP_MAIL001MT_Model; 
- 
